549a3a2488db16022a1990d856602148f9d45fe7
Yordan Dimitrov <68155186+FlameFenix@users.noreply.github.com>
2021-04-23 22:11:22 +0300

    Lab

---
+++ Recursive Array Sum/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace Recursive_Array_Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = Console.ReadLine()
+                                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                                .Select(int.Parse)
+                                .ToArray();
+
+            int sum = RecursiveSum(nums, 0);
+
+            Console.WriteLine(sum);
+        }
+
+        public static int RecursiveSum(int[] nums, int index)
+        {
+            if (index == nums.Length)
+            {
+                return 0;
+            }
+
+            return nums[index] + RecursiveSum(nums, index + 1);
+        }
+    }
+}
---
+++ Recursive Drawing/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Text;
+
+namespace Recursive_Drawing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            RecursiveDrawing(n);
+        }
+
+        static void RecursiveDrawing(int n)
+        {
+            if(n == 0)
+            {
+                return;
+            }
+
+            Console.WriteLine(new string('*', n));
+
+            RecursiveDrawing(n - 1);
+
+            Console.WriteLine(new string('#', n));
+        }
+    }
+}
