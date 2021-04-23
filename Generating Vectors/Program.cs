98c9706b2d94a08251754fb754489d4a774616dc
Yordan Dimitrov <68155186+FlameFenix@users.noreply.github.com>
2021-04-23 23:25:38 +0300

    Lab

---
+++ Generating Vectors/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Generating_Vectors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int[] array = new int[n];
+
+            Generate(0, array);
+        }
+
+        static void Generate(int index, int[] array)
+        {
+            if(index == array.Length)
+            {
+                Console.WriteLine(string.Join("", array));
+            }
+            else
+            {
+                for (int i = 0; i <= 1; i++)
+                {
+                    array[index] = i;
+                    Generate(index + 1, array);
+                }
+            }
+        }
+    }
+}
