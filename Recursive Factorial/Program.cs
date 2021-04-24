e8fa3cde515cfe57d1755cb08fca1ac9f8bf0cb9
Yordan Dimitrov <68155186+FlameFenix@users.noreply.github.com>
2021-04-24 08:37:43 +0300

    Lab

---
+++ Recursive Factorial/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Recursive_Factorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long n = long.Parse(Console.ReadLine());
+
+            Console.WriteLine(Factorial(n));
+        }
+
+        static long Factorial(long n)
+        {
+            if(n == 0)
+            {
+                return 1;
+            }
+            else
+            {
+                return n * Factorial(n - 1);
+            }
+        }
+    }
+}
