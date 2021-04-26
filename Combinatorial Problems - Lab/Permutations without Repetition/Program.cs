058d131791ea314e53898fcdc85c8cbfc443a36f
Yordan Dimitrov <68155186+FlameFenix@users.noreply.github.com>
2021-04-26 23:25:50 +0300

    Lab

---
+++ Combinatorial Problems - Lab/Permutations without Repetition/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Permutations_without_Repetition
+{
+    class Program
+    {
+        private static bool[] used;
+        private static string[] characters;
+        private static string[] permutations;
+
+        static void Main(string[] args)
+        {
+           characters = Console.ReadLine().Split().ToArray();
+
+            permutations = new string[characters.Length];
+
+            used = new bool[characters.Length];
+
+            for (int i = 0; i < used.Length; i++)
+            {
+                used[i] = false;
+            }
+
+            Permutations(0);
+
+        }
+
+        private static void Permutations(int index)
+        {
+            if (index == characters.Length)
+            {
+                Console.WriteLine(string.Join(" ", permutations));
+                return;
+            }
+
+            for (int i = 0; i < characters.Length; i++)
+            {
+                if(used[i] == false)
+                {
+                    used[i] = true;
+                    permutations[index] = characters[i];
+                    Permutations(index + 1);
+                    used[i] = false;
+                }
+                
+            }
+        }
+    }
+}
