83a3c92f58b1a0c0eac13006f0ab907e26121165
Yordan Dimitrov <68155186+FlameFenix@users.noreply.github.com>
2021-05-04 11:30:58 +0300

    Exercises

---
+++ Searching, Sorting and Greedy Algorithms - Lab/Bubble Sort/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+
+namespace Bubble_Sort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = Console.ReadLine()
+                               .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                               .Select(int.Parse)
+                               .ToArray();
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                for (int j = 1; j < arr.Length - i; j++)
+                {
+                    int firstElement = arr[j - 1];
+                    int secondElement = arr[j];
+
+                    if(firstElement > secondElement)
+                    {
+                        Swap(arr, j - 1, j);
+                    }
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", arr));
+        }
+
+        private static void Swap(int[] arr, int first, int second)
+        {
+            int backup = arr[first];
+            arr[first] = arr[second];
+            arr[second] = backup;
+        }
+    }
+}
