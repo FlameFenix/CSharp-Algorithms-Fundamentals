3518a680c37cd6646ef12bf7d80c6dc6671cc532
Yordan Dimitrov <68155186+FlameFenix@users.noreply.github.com>
2021-05-04 10:28:44 +0300

    Exercises

---
+++ Searching, Sorting and Greedy Algorithms - Lab/Binary Search/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Linq;
+
+namespace Binary_Search
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = Console.ReadLine()
+                                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                                .Select(int.Parse)
+                                .ToArray();
+
+            int target = int.Parse(Console.ReadLine());
+
+            int startIdx = 0;
+            int endIdx = nums.Length - 1;
+
+            int result = BinarySearching(nums, startIdx, endIdx, target);
+
+            Console.WriteLine(result);
+
+        }
+
+        private static int BinarySearching(int[] nums, int startIdx, int endIdx, int target)
+        {
+            while (startIdx <= endIdx)
+            {
+                int mid = (startIdx + endIdx) / 2;
+                int currentElement = nums[mid];
+
+                if (currentElement == target)
+                {
+                    return mid;
+                }
+
+                if (target > currentElement)
+                {
+                    startIdx = mid + 1;
+                }
+                else if (target < currentElement)
+                {
+                    endIdx = mid - 1;
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            return -1;
+        }
+    }
+}
