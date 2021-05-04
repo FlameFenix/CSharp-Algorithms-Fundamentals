fd1df431c63cdd7d9de75b38c46ec19e0489b423
Yordan Dimitrov <68155186+FlameFenix@users.noreply.github.com>
2021-05-04 11:58:46 +0300

    Exercises

---
+++ Searching, Sorting and Greedy Algorithms - Lab/Insertion Sort/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Insertion_Sort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = Console.ReadLine()
+                               .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                               .Select(int.Parse)
+                               .ToArray();
+
+            for (int i = 1; i < nums.Length; i++)
+            {
+                int j = i;
+                while (j > 0)
+                {
+                    int firstElement = nums[j - 1];
+                    int secondElement = nums[j];
+
+                    if(secondElement < firstElement)
+                    {
+                        Swap(nums, j - 1, j);
+                    }
+
+                    j--;
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", nums));
+        }
+
+        private static void Swap(int[] nums, int first, int second)
+        {
+            int backup = nums[first];
+            nums[first] = nums[second];
+            nums[second] = backup;
+        }
+    }
+}
