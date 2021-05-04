d020bad0e39afe2aaede9667880d58a148faebf2
Yordan Dimitrov <68155186+FlameFenix@users.noreply.github.com>
2021-05-04 10:46:33 +0300

    Exercises

---
+++ Searching, Sorting and Greedy Algorithms - Lab/Selection Sort/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+
+namespace Selection_Sort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = Console.ReadLine()
+                               .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                               .Select(int.Parse)
+                               .ToArray();
+
+            for (int i = 0; i < arr.Length; i++)
+            {                
+                for (int j = i + 1; j < arr.Length; j++)
+                {
+                    int firstElement = arr[i];
+                    int secondElement = arr[j];
+
+                    if(firstElement > secondElement)
+                    {
+                        Swap(arr, i, j);
+                    }
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", arr));
+        }
+
+        static void Swap(int[] arr , int first, int second)
+        {
+            int backup = arr[first];
+            arr[first] = arr[second];
+            arr[second] = backup;
+        }
+    }
+}
