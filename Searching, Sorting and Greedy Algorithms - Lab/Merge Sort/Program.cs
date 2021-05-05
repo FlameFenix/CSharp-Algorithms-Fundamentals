f7d88901039657e669cef33871f0b1f5220f7a6e
Yordan Dimitrov <68155186+FlameFenix@users.noreply.github.com>
2021-05-05 19:21:59 +0300

    Exercises

---
+++ Searching, Sorting and Greedy Algorithms - Lab/Merge Sort/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Linq;
+
+namespace Merge_Sort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = Console.ReadLine()
+                               .Split()
+                               .Select(int.Parse)
+                               .ToArray();
+
+            Sort(arr, 0, arr.Length - 1);
+
+            Console.WriteLine(string.Join(" ", arr));
+        }
+
+        static void Sort(int[] arr, int startIdx, int endIdx)
+        {
+            if (startIdx >= endIdx)
+            {
+                return;
+            }
+
+            int middleIdx = (startIdx + endIdx) / 2;
+
+            Sort(arr, startIdx, middleIdx);
+
+            Sort(arr, middleIdx + 1, endIdx);
+
+            Merge(arr, startIdx, middleIdx, endIdx);
+
+        }
+
+        static void Merge(int[] arr, int startIdx, int middleIdx, int endIdx)
+        {
+            if (middleIdx < 0 ||
+                middleIdx >= arr.Length ||
+                arr[middleIdx] <= arr[middleIdx + 1])
+            {
+                return;
+            }
+
+            int[] originalArray = new int[arr.Length];
+
+            int leftIdx = startIdx;
+            int rightIdx = middleIdx + 1;
+
+            for (int i = startIdx; i <= endIdx; i++)
+            {
+                originalArray[i] = arr[i];
+            }
+
+            for (int i = startIdx; i <= endIdx; i++)
+            {
+                if (leftIdx > middleIdx)
+                {
+                    arr[i] = originalArray[rightIdx++];
+                }
+                else if (rightIdx > endIdx)
+                {
+                    arr[i] = originalArray[leftIdx++];
+                }
+                else if (originalArray[leftIdx] <= originalArray[rightIdx])
+                {
+                    arr[i] = originalArray[leftIdx++];
+                }
+                else
+                {
+                    arr[i] = originalArray[rightIdx++];
+                }
+            }
+        }
+    }
+}
