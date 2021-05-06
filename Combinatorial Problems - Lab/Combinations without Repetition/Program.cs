719ea3544bcbcc7704ecef769d4475db2cbbf68b
Yordan Dimitrov <68155186+FlameFenix@users.noreply.github.com>
2021-05-06 13:09:00 +0300

    Lab

---
+++ Combinatorial Problems - Lab/Combinations without Repetition/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+
+namespace Combinations_without_Repetition
+{
+    class Program
+    {
+        static string[] elements;
+        static string[] combinations;
+
+        static void Main(string[] args)
+        {
+            elements = Console.ReadLine()
+                              .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                              .ToArray();
+            int k = int.Parse(Console.ReadLine());
+
+            combinations = new string[k];
+
+            Combinations(0, 0);
+        }
+
+        static void Combinations(int index, int loopIdx)
+        {
+            if(index >= combinations.Length)
+            {
+                Console.WriteLine(string.Join(" ", combinations));
+                return;
+            }
+
+            for (int i = loopIdx; i < elements.Length; i++)
+            {
+                combinations[index] = elements[i];
+                Combinations(index + 1, i + 1);
+            }
+        }
+    }
+}
