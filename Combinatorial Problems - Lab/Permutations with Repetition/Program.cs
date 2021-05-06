f616f6a0b9309f5d30b80fc87e980fa1468c82b6
Yordan Dimitrov <68155186+FlameFenix@users.noreply.github.com>
2021-05-06 12:30:32 +0300

    Lab

---
+++ Combinatorial Problems - Lab/Permutations with Repetition/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Permutations_with_Repetition
+{
+    class Program
+    {
+        static char[] elements;
+        static HashSet<string> setElements;
+
+        static void Main(string[] args)
+        {
+            elements = Console.ReadLine()
+                              .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                              .Select(char.Parse)
+                              .ToArray();
+
+            setElements = new HashSet<string>();
+
+            Permute(0);
+
+            foreach (var item in setElements)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        static void Permute(int index)
+        {
+            if (index >= elements.Length)
+            {
+                setElements.Add(string.Join(" ", elements));
+                return;
+            }
+
+            Permute(index + 1);
+
+            HashSet<char> set = new HashSet<char>() { elements[index] };
+
+            for (int i = index + 1; i < elements.Length; i++)
+            {
+                int first = index;
+                int second = i;
+                if(!set.Contains(elements[i]))
+                {
+                    Swap(first, second);
+                    Permute(index + 1);
+                    Swap(first, second);
+                    set.Add(elements[i]);
+                }
+            }
+
+        }
+
+        static void Swap(int first, int second)
+        {
+            char backup = elements[first];
+            elements[first] = elements[second];
+            elements[second] = backup;
+        }
+    }
+}
---
+++ Combinatorial Problems - Lab/Variations with Repetitions/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+
+namespace Variations_with_Repetitions
+{
+    class Program
+    {
+        private static char[] elements;
+        private static char[] variations;
+
+        static void Main(string[] args)
+        {
+            elements = Console.ReadLine()
+                              .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                              .Select(char.Parse)
+                              .ToArray();
+
+            int k = int.Parse(Console.ReadLine());
+
+            variations = new char[k];
+
+            for (int i = 0; i < variations.Length; i++)
+            {
+                variations[i] = elements[i];
+            }
+
+            Variations(0);
+        }
+
+        private static void Variations(int index)
+        {
+            if (index >= variations.Length)
+            {
+                Console.WriteLine(string.Join(" ", variations));
+                return;
+            }
+
+            for (int i = 0; i < elements.Length; i++)
+            {
+                variations[index] = elements[i];
+                Variations(index + 1);
+            }
+        }
+    }
+}
