619f7827d752368a90ef36b4a6c4867ec864385c
Yordan Dimitrov <68155186+FlameFenix@users.noreply.github.com>
2021-05-07 00:02:03 +0300

    Exercises

---
+++ Recursion and Combinatorial Problems - Exercise/Combinations without Repetition/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Combinations_without_Repetition
+{
+    class Program
+    {
+        static int[] elements;
+        static int[] combinations;
+
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int k = int.Parse(Console.ReadLine());
+
+            elements = new int[n];
+            combinations = new int[k];
+
+            for (int i = 0; i < elements.Length; i++)
+            {
+                elements[i] = i + 1;
+            }
+
+            Combinations(0, 0);
+        }
+
+        private static void Combinations(int index, int loopIdx)
+        {
+            if (index >= combinations.Length)
+            {
+                Console.WriteLine(string.Join(" ", combinations));
+                return;
+            }
+
+            for (int i = loopIdx; i < elements.Length; i++)
+            {
+                combinations[index] = elements[i];
+                Combinations(index + 1, i + 1);
+            }
+        }
+    }
+}
---
+++ Recursion and Combinatorial Problems - Exercise/Nested Loops To Recursion/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Nested_Loops_To_Recursion
+{
+    class Program
+    {
+        static int[] nums;
+        static int[] combination;
+
+        static void Main(string[] args)
+        {
+            int k = int.Parse(Console.ReadLine());
+
+            nums = new int[k];
+            combination = new int[k];
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                nums[i] = i + 1;
+            }
+
+            NestedToRecursion(nums, 0);
+        }
+
+        private static void NestedToRecursion(int[] nums, int index)
+        {
+            if (index >= nums.Length)
+            {
+                Console.WriteLine(string.Join(" ", combination));
+                return;
+            }
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                combination[index] = nums[i];
+                NestedToRecursion(nums, index + 1);
+            }
+        }
+    }
+}
