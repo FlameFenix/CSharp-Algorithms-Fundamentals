9d86c5cf388ab74fe736f184c98b4331ba9a5f7f
Yordan Dimitrov <68155186+FlameFenix@users.noreply.github.com>
2021-05-06 15:12:58 +0300

    Exercises

---
+++ Recursion and Combinatorial Problems - Exercise/Reverse Array/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace Reverse_Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] arr = Console.ReadLine().Split().ToArray();
+
+            Console.WriteLine(ReverseArray(arr, arr.Length - 1));
+        }
+
+        static string ReverseArray(string[] arr, int index)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            if(index < 0)
+            {
+                return sb.ToString();
+            }
+
+            sb.Append(arr[index]);
+
+            return sb.ToString() + " " + ReverseArray(arr, index - 1);
+        }
+    }
+}
